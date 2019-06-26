438bed06e19abc99e25a9d5c50d8f80676e9c041
xsu <xsu@thoughtworks.com>
2019-06-26 16:09:23 +0800

    fixed compile error.

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -2,7 +2,7 @@ package com.tws.refactoring;
 
 public class Police {
     public boolean checkDriver(Driver driver) {
-        if(driver.age >= 18) return true;
+        if(driver.getAge() >= 18) return true;
         else return false;
     }
 }
