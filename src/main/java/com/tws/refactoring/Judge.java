2fcdfb46bdc6e435942645ae471e08d840558e61
suxin <xloypaypa@gmail.com>
2019-07-15 23:59:17 +0800

    judge

--- src/main/java/com/tws/refactoring/Judge.java
+++ src/main/java/com/tws/refactoring/Judge.java
@@ -8,18 +8,14 @@ public class Judge {
         已满十四周岁不满十八周岁的人犯罪，应当从轻或者减轻处罚。
         因不满十六周岁不予刑事处罚的，责令他的家长或者监护人加以管教；在必要的时候，也可以由政府收容教养。
      */
-    public CriminalResponsibility checkCriminalResponsibility(Driver driver) {
+    public boolean checkCriminalResponsibility(Driver driver, boolean isSerious) {
         if (driver.getAge() < 14) {
-            return CriminalResponsibility.NONE;
+            return false;
         } else if (driver.getAge() >= 14 && driver.getAge() < 16) {
-            return CriminalResponsibility.RELATIVE;
+            return isSerious;
         } else {
-            return CriminalResponsibility.FULL;
+            return true;
         }
-    }
-
-    public enum CriminalResponsibility {
-        NONE, FULL, RELATIVE
     }
 
 }
