18ae055faaa2c38f172cbfe922e200926687f8cf
suxin <xloypaypa@gmail.com>
2019-07-15 23:45:24 +0800

    judge

--- src/main/java/com/tws/refactoring/Judge.java
+++ src/main/java/com/tws/refactoring/Judge.java
@@ -2,6 +2,12 @@ package com.tws.refactoring;
 
 public class Judge {
 
+    /*
+        第十七条    已满十六周岁的人犯罪，应当负刑事责任。
+        已满十四周岁不满十六周岁的人，犯故意杀人、故意伤害致人重伤或者死亡、强奸、抢劫、贩卖毒品、放火、爆炸、投毒罪的，应当负刑事责任。
+        已满十四周岁不满十八周岁的人犯罪，应当从轻或者减轻处罚。
+        因不满十六周岁不予刑事处罚的，责令他的家长或者监护人加以管教；在必要的时候，也可以由政府收容教养。
+     */
     public CriminalResponsibility checkCriminalResponsibility(Driver driver) {
         if (driver.getAge() < 14) {
             return CriminalResponsibility.NONE;
